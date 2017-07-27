7c89775f24e4cf8285469418562802569ae2398c
mingyiz <mingyi.z@outlook.com>
2017-07-27 13:30:52 +0800

    change version to 0.2.0

--- packages/custom.py
+++ packages/custom.py
@@ -6,5 +6,5 @@ from cerbero.enums import License
 class Ribbon:
 
     url = "http://www.gnu.org/licenses/licenses.html"
-    version = '0.1.0'
+    version = '0.2.0'
     SDKs={'gstreamer-1.0':'1.12.2-1'}
